0850d4b0d43c807d939dee266d85f611bc6d4fbc
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-10-04 23:43:52 +0100

    adding meals route
    adding new get route,
    
    it returns the meals,

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ app = FastAPI()
 async def home () -> dict [str, str]:
     return {"home":"this is home"}
 
+
+
+
+MEALS =\
+[
+    {'id':1, 'name':'meal1'},
+    {'id':2, 'name':'meal2'},
+    {'id':3, 'name':'meal3'},
+]
+
+@app.get ('/meals')
+async def meals () -> list[dict]:
+    return MEALS
