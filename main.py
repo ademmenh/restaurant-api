d87e527d0419b44157a9366a7932ce58e41b10fc
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-10-06 02:12:28 +0100

    using pydantic
    removing unnecessary script
    
    the -> Meal is the one that define the type in the documentation
    
    removing Meal (**meal) this is performance killer,
    
    removing type checking because pydantic do it by default

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def home () -> dict [str, str]:
 
 @app.get ('/meals')
 async def Meals () -> list[Meal] :
-    return [ Meal(**meal) for meal in MEALS ]
+    return [ meal for meal in MEALS ]
 
 
 @app.get ('/meals/id/{mid}')
@@ -36,13 +36,12 @@ async def MealMid (mid : int) -> Meal :
                 return meal
                 break
         raise HTTPException (status_code=404, detail=f'the mid {mid} do not exist!')
-    else:
-        raise HTTPException (status_code=404, detail='the mid must be string!')
+
 
 
 @app.get ('/meals/genre/{genre}')
 async def MealGenre ( genre:EnumGetGenre ) -> list[Meal]:
-    return [ Meal(**meal) for meal in MEALS if meal['genre'].lower() == genre.value ]
+    return [ meal for meal in MEALS if meal['genre'].lower() == genre.value ]
 
 
 @app.get ('/meals/name/{name}')
@@ -53,5 +52,7 @@ async def MealName ( name:str ) -> Meal:
             break
     
     raise HTTPException (status_code=404, detail=f'the meal {name} do not exist !')
+
+
 
 
