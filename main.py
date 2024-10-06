e0b4fa75e5f12ce4d060c4865f78a81fdcc0872e
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-10-06 13:36:46 +0100

    new validation edit
    bring back Meal(**meal) for pydantic validation,
    
    removing isinstance ()

--- main.py
+++ main.py
@@ -2,16 +2,19 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
 
-from .schemas.enums import *
-from .schemas.data import *
+from .schemas.enums import EnumGetGenre
+from .schemas.data import MEALS
 
-from .middlewares.validation import *
-
-
+from .middlewares.validation import Meal
 
 
 
 app = FastAPI()
+
+
+
+
+
 
 
 
@@ -23,34 +26,40 @@ async def home () -> dict [str, str]:
 
 
 
+
+
+
 @app.get ('/meals')
+
 async def Meals () -> list[Meal] :
     return [ meal for meal in MEALS ]
 
 
-@app.get ('/meals/id/{mid}')
+
+@app.get ('/meals/mid/')
+
 async def MealMid (mid : int) -> Meal :
-    if isinstance (mid, int):
-        for meal in MEALS:
-            if meal['id']==mid :
-                return meal
-                break
-        raise HTTPException (status_code=404, detail=f'the mid {mid} do not exist!')
+    for meal in MEALS:
+        if meal['id']==mid :
+            return Meal(**meal)
+    raise HTTPException (status_code=404, detail=f'the mid {mid} do not exist!')
 
 
 
 @app.get ('/meals/genre/{genre}')
+
 async def MealGenre ( genre:EnumGetGenre ) -> list[Meal]:
-    return [ meal for meal in MEALS if meal['genre'].lower() == genre.value ]
+    return [ Meal(**meal) for meal in MEALS if meal['genre'].lower() == genre.value ]
+
 
 
 @app.get ('/meals/name/{name}')
+
 async def MealName ( name:str ) -> Meal:
     for meal in MEALS:
         if meal['name'] == name:
-            return meal
+            return Meal(**meal)
             break
-    
     raise HTTPException (status_code=404, detail=f'the meal {name} do not exist !')
 
 
