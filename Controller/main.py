6774e7071e939c22ad8da2c56d9bb77c37b9b22c
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-10-13 16:11:28 +0100

    testing authentication,
    this returns unauthenticated

--- Controller/main.py
+++ Controller/main.py
@@ -1,12 +1,12 @@
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from contextlib import asynccontextmanager
 
 from .middlewares.pydantic import Meal, GETMeal, POSTMeal
 
 from ..Model import orm
 
-
+from fastapi.security import OAuth2PasswordBearer
 
 
 
@@ -16,21 +16,31 @@ async def db_lifespan (app: FastAPI):
     yield
 
 
+
+
+
 app = FastAPI(lifespan=db_lifespan)
 
 
 
-
-
-@app.get ('/')
+@app.get ('/home')
 async def home () -> dict [str, str]:
     return {"home":"this is home"}
+
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+@app.get ('/auth')
+async def auth (token=Depends(oauth2_scheme)):
+    return token
 
 
 
 @app.get ('/meals')
 async def meals ( meal:GETMeal ) -> list[Meal]:
     return orm.Meal.get_meal(meal.__dict__)
+
 
 
 @app.post ('/meals')
